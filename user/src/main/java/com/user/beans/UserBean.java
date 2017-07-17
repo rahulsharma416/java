32330685f7026ca109ec7236159c91ec0fcd393a
Rahul Sharma <rahul.sharma@xcubelabs.com>
2017-07-17 23:43:42 +0530

    Usermaintenance with hibernate

---
+++ user/src/main/java/com/user/beans/UserBean.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.user.beans;
+
+/**
+ *
+ * @author Aravind
+ */
+public class UserBean {
+
+   private String userId;
+   private String username;
+   private String password;
+   private String email;
+   private String status;
+
+   public UserBean(String userId, String username, String password, String email, String status) {
+      this.userId = userId;
+      this.username = username;
+      this.password = password;
+      this.email = email;
+      this.status = status;
+   }
+
+   public String getUserId() {
+      return userId;
+   }
+
+   public void setUserId(String userId) {
+      this.userId = userId;
+   }
+
+   public String getUsername() {
+      return username;
+   }
+
+   public void setUsername(String username) {
+      this.username = username;
+   }
+
+   public String getPassword() {
+      return password;
+   }
+
+   public void setPassword(String password) {
+      this.password = password;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public String getStatus() {
+      return status;
+   }
+
+   public void setStatus(String status) {
+      this.status = status;
+   }
+   
+}
