64b5acf936a8b2ee93f951aef609a3249db6af5f
Rahul Sharma <rahul.sharma@xcubelabs.com>
2017-08-28 14:25:39 +0530

    comparepcp

---
+++ dev.comparepcp.com/Customer/GetModels.php
@@ -0,0 +1,8 @@
+<?php
+$brand = $_POST['Brand'];
+
+$models = array('A1', 'A3', 'A3 Saloon', 'A3 Sportback', 'A4 Avant', 'A4 Saloon', 'A5 Cabriolet', 'A5 Coupe',
+    'A5 Sportback', 'A6 Avant', 'A6 Quattro', 'A6 Saloon', 'A7', 'Q3', 'Q7', 'S1', 'S1 Sportback', 'S6 Saloon', 'S7', 
+    'TT');
+
+echo json_encode($models);
---
+++ dev.comparepcp.com/Customer/GetTrims.php
@@ -0,0 +1,23 @@
+<?php
+$trims = array('Black Edition 1.4 TFSI cylinder on demand Manual',
+'Black Edition 1.4 TFSI cylinder on demand S tronic',
+'Black Edition 1.6 TDI Manual',
+'Black Edition 1.6 TDI S tronic',
+'S line 1.4 TFSI cylinder on demand Manual',
+'S line 1.4 TFSI cylinder on demand S tronic',
+'S line 1.4 TFSI Manual',
+'S line 1.4 TFSI S tronic',
+'S line 1.6 TDI Manual',
+'S line 1.6 TDI S tronic',
+'SE 1.0 TFSI Manual',
+'SE 1.0 TFSI S tronic',
+'SE 1.6 TDI Manual',
+'SE 1.6 TDI S tronic',
+'Sport 1.0 TFSI Manual',
+'Sport 1.0 TFSI S tronic',
+'Sport 1.4 TFSI Manual',
+'Sport 1.4 TFSI S tronic',
+'Sport 1.6 TDI Manual',
+'Sport 1.6 TDI S tronic');
+
+print(json_encode($trims));
