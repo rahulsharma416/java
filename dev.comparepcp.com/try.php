2220a7bc3108e0af9a6ae18154ea81ddc2c20016
Rahul Sharma <rahul.sharma@xcubelabs.com>
2017-09-02 00:05:14 +0530

    Material design

---
+++ dev.comparepcp.com/try.php
@@ -0,0 +1,36 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<div id="overlay"></div>
+		<div class="fab">
+		  <i class="material-icons fab-icon">add</i>
+
+		  <form class='cntt-wrapper'>
+			<div id="fab-hdr">
+			  <h3>Popup</h3>
+			</div>
+
+			<div class="cntt">
+			  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="text" id="text1" />
+				<label class="mdl-textfield__label" for="text2">Text...</label>
+			  </div>
+
+			  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="text" id="text2" />
+				<label class="mdl-textfield__label" for="text2">More text...</label>
+			  </div>
+			</div>
+
+			<div class="btn-wrapper">
+			  <button class="mdl-button mdl-js-button" id="cancel">Cancel</button>
+			  <button class="mdl-button mdl-js-button mdl-button--primary" id="submit">Submit</button>
+			</div>
+
+		  </form>
+		</div>
+
+
+	</body>
+</html>
