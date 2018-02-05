3bf1c4cd36dfe2d78810589c1dc2b6803b9d8088
Rahul Sharma <rahul.sharma@xcubelabs.com>
2018-02-05 09:42:34 +0530

    COmments

--- user/src/main/java/com/user/UserApplication.java
+++ user/src/main/java/com/user/UserApplication.java
@@ -3,6 +3,9 @@ package com.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+* Application starting point
+*/
 @SpringBootApplication
 public class UserApplication {
 
